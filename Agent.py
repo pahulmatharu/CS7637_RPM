fa32773f8a414b6cb055f041ec3604f0bcf96496
pmatharu-irismedtech <156706743+pmatharu-irisai@users.noreply.github.com>
2024-03-11 01:26:25 -0400

    b

--- Agent.py
+++ Agent.py
@@ -30,6 +30,7 @@ figure_index_8 = '8'
 ROTATE = 'rotate'
 FLIP = 'flip'
 SAME = 'same'
+FILL_WHOLE_IMG = 'FILL_WHOLE_IMG'
 
 class Agent:
     def __init__(self):
@@ -47,6 +48,19 @@ class Agent:
         gImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gTemplate = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
         return (cv2.matchTemplate(gImage, gTemplate, cv2.TM_CCOEFF_NORMED).max()) * 100
+    
+    def full_img_fill(self, filename, filename_to_compare):
+        img = cv2.imread(filename) 
+        img_template = cv2.imread(filename_to_compare) 
+        _, im_th = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY_INV)
+        im_floodfill = im_th.copy()
+        h, w = im_th.shape[:2]
+        mask = np.zeros((h+2, w+2), np.uint8)
+        cv2.floodFill(im_floodfill, mask, (0,0), (255,255,255))
+        sim = self.similarity(im_floodfill, img_template)
+        if(sim > 94):
+            return (True, sim)
+        return (False, None)
     
     def IsSame2x2(self, image_one_filname, image_two_filname):
         image = cv2.imread(image_one_filname)
@@ -150,8 +164,28 @@ class Agent:
                 generated_answers.append(GeneratedAnswer(similarity= sim, answerIndex= index + 1, method=SAME))
         return (image, generated_answers)
     
+    def generate_fill2x2(self, answers, image_for_generation=None, image_object=None):
+        image = None
+        if(image_for_generation is None):
+            image = image_object
+        else:
+            image = cv2.imread(image_for_generation)
+ 
+        _, im_th = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY_INV)
+        im_floodfill = im_th.copy()
+        h, w = im_th.shape[:2]
+        mask = np.zeros((h+2, w+2), np.uint8)
+        cv2.floodFill(im_floodfill, mask, (0,0), (255,255,255))
+        
+        generated_answers = []
+        for index,answer in enumerate(answers):
+                answerImage = cv2.imread(answer)
+                sim = self.similarity(im_floodfill, answerImage)
+                generated_answers.append(GeneratedAnswer(similarity= sim, answerIndex= index + 1, method=FILL_WHOLE_IMG))
+        return (image, generated_answers)
+    
     def generate_images(self, method, answers, image_filename=None, imageObject=None):
-        print('---generating answer images----', method)
+        # print('---generating answer images----', method)
         method_type = method[0]
         method_amount = method[1]
         if(method_type == SAME):
@@ -160,6 +194,8 @@ class Agent:
             return self.generate_rotation2x2(image_for_generation=image_filename, image_object=imageObject, answers=answers, rotationAmount=method_amount)
         if(method_type == FLIP):
             return self.generate_flip2x2(image_for_generation=image_filename, image_object=imageObject, flipDirection=method_amount, answers=answers)
+        if(method_type == FILL_WHOLE_IMG):
+            return self.generate_fill2x2(image_for_generation=image_filename, image_object=imageObject, answers=answers)
 
     def generate_and_test(self, a_b_methods, a_c_methods, answers, imageB, imageC):
         a_b_len = len(a_b_methods)
@@ -174,7 +210,7 @@ class Agent:
                 ans_row_list.extend(images[1])
             # column
             ans_column_list = []           
-            for method_column in a_b_methods:
+            for method_column in a_c_methods:
                 images_column = self.generate_images(method=method_column, image_filename=imageB, answers=answers)
                 ans_column_list.extend(images_column[1])
 
@@ -197,8 +233,8 @@ class Agent:
                 ans_list.extend(images[1])
             if(len(ans_list) > 1):    
                 ans_list.sort(key=lambda ans: ans.similarity, reverse=True)
-                # for i in ans_list:
-                #     print(i.method, i.similarity, i.answerIndex)
+                for i in ans_list:
+                    print(i.method, i.similarity, i.answerIndex)
                 return ans_list[0].answerIndex
         
         elif(a_c_len > 0):
@@ -211,8 +247,8 @@ class Agent:
 
             if(len(ans_list) > 1):    
                 ans_list.sort(key=lambda ans: ans.similarity, reverse=True)
-                # for i in ans_list:
-                #     print(i.method, i.similarity, i.answerIndex)
+                for i in ans_list:
+                    print(i.method, i.similarity, i.answerIndex)
                 return ans_list[0].answerIndex
 
         return 1
@@ -235,7 +271,6 @@ class Agent:
         #     image_1 = cv2.imread(problem.figures["1"].visualFilename)
 
         # Don't forget to uncomment the import!
-
         name = problem.name
         problemType = problem.problemType
         problemSetName = problem.problemSetName
@@ -283,11 +318,21 @@ class Agent:
             A_B_methods.extend(rotation_methodsA_B)
             A_C_methods.extend(rotation_methodsA_C)
 
-            # check rotation
+            # check flip
             flip_methodsA_B = self.flipAxis2x2(figures[A].visualFilename, figures[B].visualFilename)
             flip_methodsA_C = self.flipAxis2x2(figures[A].visualFilename, figures[C].visualFilename)
             A_B_methods.extend(flip_methodsA_B)
             A_C_methods.extend(flip_methodsA_C)
+            
+            # check if fill whole image works
+            img_fill_row = self.full_img_fill(figures[A].visualFilename, figures[B].visualFilename)
+            if(img_fill_row[0]):
+                A_B_methods.append((FILL_WHOLE_IMG, 'h', img_fill_row[1]))
+            img_fill_col = self.full_img_fill(figures[A].visualFilename, figures[C].visualFilename)
+            if(img_fill_col[0]):
+                A_C_methods.append((FILL_WHOLE_IMG, 'v', img_fill_col[1]))
+                
+            # check dark pixel count difference
             
             # Generate and Test
             if(len(A_B_methods) > 1):    
@@ -295,10 +340,8 @@ class Agent:
             if(len(A_C_methods) > 1):    
                 A_C_methods.sort(key=lambda tup: tup[2], reverse=True)
             answer_index = self.generate_and_test(A_B_methods, A_C_methods, answers, figures[B].visualFilename, figures[C].visualFilename)
-            print('answer_found', answer_index)
+            print('answwe', answer_index)
 
-            # Setup Image Class for each image
-            # ravenImageA = RavenImage(figures[A].visualFilename)
             return answer_index
         
             
@@ -335,76 +378,118 @@ class GeneratedAnswer:
         self.answerIndex = answerIndex
         
 
-class RavenImage:
-    def __init__(self, filename):
-        shapes = self.get_shapes(filename)
-        self.filename = filename
-        self.shapes = shapes
-    
-    def get_shapes(self, filename):
+class ShapeDetector:
+    def __init__(self, image_a_filename, image_b_filename, image_c_filename):
+        pass
+        
+
+    def get_shapes_count(self, filename):
         # reading image 
         img = cv2.imread(filename) 
+        
+        # # convert to gray
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
         
-        # setting threshold of gray image 
-        ret, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY) 
+        # # setting threshold of gray image 
+        _, threshold_image = cv2.threshold(gray, 15, 255, cv2.THRESH_BINARY) 
         
-        # using a findContours() function 
-        contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        print('hier', hierarchy)
+        # # using a findContours() function 
+        contours, hierarchy = cv2.findContours(threshold_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # # print('hier', hierarchy)
+        
         i = 0
-        shapes = []
-        # list for storing names of shapes 
+        
+        triangle_count = 0
+        quad_count = 0
+        penta_count = 0
+        hexa_count = 0
+        seven_count = 0
+        eight_count = 0
+        circle_count = 0
+        # # list for storing names of shapes 
         for contour in contours: 
-            # here we are ignoring first counter because  
-            # findcontour function detects whole image as shape 
+        #     # here we are ignoring first counter because  
+        #     # findcontour function detects whole image as shape 
             if i == 0: 
                 i = 1
                 continue
-        
-            if cv2.contourArea(contour) > 0:
-                print('filled')
-            # cv2.approxPloyDP() function to approximate the shape 
-            approx = cv2.approxPolyDP( 
-                contour, 0.01 * cv2.arcLength(contour, True), True) 
             
-            # using drawContours() function 
-            cv2.drawContours(img, [contour], 0, (0, 0, 255), 5) 
-        
+            epsilon = 0.01 * cv2.arcLength(contour, True)
+            # cv2.approxPloyDP() function to approximate the shape 
+            approx = cv2.approxPolyDP(contour, epsilon, True) 
+            
+           # using drawContours() function 
+            # img_cp = img.copy()
+            # cv2.drawContours(img_cp, [contour], 0, (0, 0, 0), -1)
+            # sim = self.sim(img_cp, img_template)
+            # cv2.imshow('cp', img_cp) 
+            # cv2.imshow('shapes', img) 
+            # cv2.waitKey(0) 
+            # cv2.destroyAllWindows()
+            
             # finding center point of shape 
-            M = cv2.moments(contour) 
-            if M['m00'] != 0.0: 
-                x = int(M['m10']/M['m00']) 
-                y = int(M['m01']/M['m00']) 
-        
+            x,y,w,h = cv2.boundingRect(approx)
+            center_x = int(x + w/2)
+            center_y = int(y + h/2)
+
 
             # putting shape name at center of each shape 
             if len(approx) == 3: 
-                triangle = Shape(x, y, 'triange')
-                shapes.append(triangle)
+                # triangle = Shape(center_x, center_y, 'triange')
+                # shapes.append(triangle)
+                triangle_count += 1
         
             elif len(approx) == 4: 
-                quad = Shape(x, y, 'quad')
-                shapes.append(quad)
-        
-            elif len(approx) == 5: 
-                penta = Shape(x,y,'penta')
-                shapes.append(penta)
-        
-            elif len(approx) == 6: 
-                hexa = Shape(x,y,'hexa')
-                shapes.append(hexa)
-        
-            else: 
-                circle = Shape(x,y, 'circle')
-                shapes.append(circle)
+                # quad = Shape(center_x, center_y, 'quad')
+                # shapes.append(quad)
+                quad_count += 1
                 
-        cv2.imshow('shapes', img) 
-        cv2.waitKey(0) 
-        cv2.destroyAllWindows() 
-        for shape in shapes:
-            print(shape.shape_type, shape.x, shape.y)
+            elif len(approx) == 5: 
+                # penta = Shape(center_x,center_y,'penta')
+                # shapes.append(penta)
+                penta_count += 1
+                
+            elif len(approx) == 6: 
+                # hexa = Shape(center_x,center_y,'hexa')
+                # shapes.append(hexa)
+                hexa_count += 1
+                
+            elif len(approx) == 7: 
+                # hexa = Shape(center_x,center_y,'seven')
+                # shapes.append(hexa)
+                seven_count += 1
+                
+            elif len(approx) == 8: 
+                # hexa = Shape(center_x,center_y,'eight')
+                # shapes.append(hexa)
+                eight_count += 1
+                
+            else: 
+                # circle = Shape(center_x,center_y, 'circle')
+                # shapes.append(circle)
+                circle_count += 1
+                
+        # for shape in shapes:
+        #     print(shape.shape_type, shape.x, shape.y)
+        return ShapeCount(triangle_count, quad_count, penta_count, hexa_count, seven_count, eight_count, circle_count)
+            
+    def sim(self, image, template):
+        gImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gTemplate = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+        return (cv2.matchTemplate(gImage, gTemplate, cv2.TM_CCOEFF_NORMED).max()) * 100
+        
 
+class ShapeCount:
+    def __init__(self, triangle_count, quad_count,penta_count,hexa_count,seven_count,eight_count, circle_count):
+        self.triangle_count = triangle_count
+        self.quad_count = quad_count
+        self.penta_count = penta_count
+        self.hexa_count = hexa_count
+        self.seven_count = seven_count
+        self.eight_count = eight_count
+        self.circle_count = circle_count
+        self.full_count = triangle_count + quad_count + penta_count + hexa_count + seven_count + eight_count + circle_count
+        
 class Shape:
     def __init__(self, x, y, shape_type):
         self.x = x
